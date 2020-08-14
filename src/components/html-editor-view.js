41583b33392595e886b3a77a050d7234cadabdb6
Henrique Macedo <mail@henriquemacedo.pt>
2020-08-14 15:41:39 +0100

    Add html view wrapper

--- src/components/html-editor-view.js
+++ src/components/html-editor-view.js
@@ -1,11 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
+import styled from "styled-components";
 
-export const HtmlEditorView = ({ value }) => <FroalaEditorView model={value} />;
+const Wrapper = styled.div`
+  h2,
+  h3 {
+    margin-bottom: 15px !important;
+  }
+
+  h2 {
+    font-size: 1.25rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+`;
+
+export const HtmlEditorView = ({ value }) => (
+  <Wrapper>
+    <FroalaEditorView model={value} />
+  </Wrapper>
+);
 
 HtmlEditorView.propTypes = {
-  value: PropTypes.string
+  value: PropTypes.string,
 };
 
 export default HtmlEditorView;
