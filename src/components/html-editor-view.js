17b99667005eca938f08fba7a7eab45700d385af
Henrique Macedo <mail@henriquemacedo.pt>
2020-09-01 12:10:31 +0100

    Remove overwrite styling from html viewer

--- src/components/html-editor-view.js
+++ src/components/html-editor-view.js
@@ -1,31 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
-import styled from "styled-components";
 
-const Wrapper = styled.div`
-  h2,
-  h3 {
-    margin-bottom: 15px !important;
-  }
-
-  h2 {
-    font-size: 1.25rem;
-  }
-
-  h3 {
-    font-size: 1rem;
-  }
-`;
-
-export const HtmlEditorView = ({ value }) => (
-  <Wrapper>
-    <FroalaEditorView model={value} />
-  </Wrapper>
-);
+export const HtmlEditorView = ({ value }) => <FroalaEditorView model={value} />;
 
 HtmlEditorView.propTypes = {
-  value: PropTypes.string,
+  value: PropTypes.string
 };
 
 export default HtmlEditorView;
